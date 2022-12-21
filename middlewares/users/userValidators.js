c71b62926a9568d9d534c3d2d9cc90d686fc2ff2
abir <abirrens@gmail.com>
2022-12-21 19:36:48 +0530

    first commit

--- middlewares/users/userValidators.js
+++ middlewares/users/userValidators.js
@@ -11,19 +11,19 @@ const User = require("../../models/People");
 const addUserValidators = [
   check("name")
     .isLength({ min: 1 })
-    .withMessage("Name is required")
+    .withMessage("Name is required*")
     .isAlpha("en-US", { ignore: " -" })
-    .withMessage("Name must not contain anything other than alphabet")
+    .withMessage("Name must not contain anything other than alphabet*")
     .trim(),
   check("email")
     .isEmail()
-    .withMessage("Invalid email address")
+    .withMessage("Invalid email address*")
     .trim()
     .custom(async (value) => {
       try {
         const user = await User.findOne({ email: value });
         if (user) {
-          throw createError("Email already is use!");
+          throw createError("Email already is use!*");
         }
       } catch (err) {
         throw createError(err.message);
@@ -33,12 +33,12 @@ const addUserValidators = [
     .isMobilePhone("en-IN", {
       strictMode: true,
     })
-    .withMessage("Mobile number must be a valid Bangladeshi mobile number")
+    .withMessage("Enter a valid mobile number*")
     .custom(async (value) => {
       try {
         const user = await User.findOne({ mobile: value });
         if (user) {
-          throw createError("Mobile already is use!");
+          throw createError("Mobile already is use*");
         }
       } catch (err) {
         throw createError(err.message);
@@ -47,7 +47,7 @@ const addUserValidators = [
   check("password")
     .isStrongPassword()
     .withMessage(
-      "Password must be at least 8 characters long & should contain at least 1 lowercase, 1 uppercase, 1 number & 1 symbol"
+      "Password must be at least 8 characters long & should contain at least 1 lowercase, 1 uppercase, 1 number & 1 symbol*"
     ),
 ];
 
